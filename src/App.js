a92b654550403fdaf8b262b7e637684b007e0316
Noah Roberts <noahmroberts@gmail.com>
2021-06-01 12:51:32 -0500

    added nav and create recipe components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import { Route, Link } from 'react-router-dom'
+import NavBar from './NavBar/NavBar'
 import './App.css'
 
 
 export default function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+      <NavBar/>
+      <h1>hello</h1>
     </div>
   );
 }
