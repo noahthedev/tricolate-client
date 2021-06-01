74c9168543143943b2a348818613634def11f0f3
Noah Roberts <noahmroberts@gmail.com>
2021-06-01 14:28:41 -0500

    created app folder

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+import './App.css'
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <h1>Brew with Tricolate</h1>
+      <p>The Tricolate is a revolutionary pourover coffee brewer that allows you to brew with zero bypass.</p>
+      <footer>Copyright 2021 Noah Roberts</footer>
+    </div>
+  );
+}
+
+
