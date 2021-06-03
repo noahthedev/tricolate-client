4bfd6752bb6fcd7d8317c3cae65d6be3453e2086
Noah Roberts <noahmroberts@gmail.com>
2021-06-03 13:59:56 -0500

    added recipe input form

--- src/CreateRecipe/CreateRecipe.js
+++ src/CreateRecipe/CreateRecipe.js
@@ -5,10 +5,35 @@ export default class CreateRecipe extends React.Component {
     return (
       <div>
         <form>
-          <label htmlFor='title-input'>Title</label>
-          <input type='text' id='title-input' />
-          <label htmlFor=''
-
+          <div>
+            <label htmlFor='title-input'>Title</label>
+            <input type='text' id='title-input' />
+          </div>
+          <div>
+            <label htmlFor='abstract-input'>Description</label>
+            <input type='text' id='abstract-input' />
+          </div>
+          <div>
+            <label htmlFor='coffee-input'>Coffee (weight in grams)</label>
+            <input type='number' id='coffee-input' />
+          </div>
+          <div>
+            <label htmlFor='grind-input'>Grind</label>
+            <input type='text' id='grind-input' />
+          </div>
+          <div>
+            <label htmlFor='water-input'>Water (weight in grams) </label>
+            <input type='number' id='water-input' />
+          </div>
+          <div>
+            <label htmlFor='method-input'>Method</label>
+            <textarea id='method-input' />
+          </div>
+          <div>
+            <label htmlFor='link-input'>Link (URL)</label>
+            <input type='url' id='link-input' />
+          </div>
+          <button type='submit'>Add recipe</button>
         </form>
       </div>
     )
